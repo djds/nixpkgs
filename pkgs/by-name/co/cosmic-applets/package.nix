880fbb627cfb87f64850e3f48e4111ce9e4bccdf
Davis Schirmer <git@djds.dev>
2025-09-23 02:50:34 -0400

    [cosmic-applets] Remove bluetooth patch; add pipewire to buildInputs

--- pkgs/by-name/co/cosmic-applets/package.nix
+++ pkgs/by-name/co/cosmic-applets/package.nix
@@ -11,6 +11,7 @@
   dbus,
   glib,
   libinput,
+  pipewire,
   pulseaudio,
   udev,
   xkeyboard_config,
@@ -32,25 +33,19 @@ rustPlatform.buildRustPackage (finalAttrs: {
 
   cargoHash = "sha256-RnkyIlTJMxMGu+EsmZwvSIapSqdng+t8bqMVsDXprlU=";
 
-  patches = [
-    (fetchpatch2 {
-      name = "fix-bluetooth-dbus-spam.patch";
-      url = "https://github.com/pop-os/cosmic-applets/commit/b6bb982f2dace0a3d19c78b4b4247760a8010d5b.patch?full_index=1";
-      hash = "sha256-S5F9rqYrB38T9R6i/n/j3s79Xeh6BMmNkC+E2kTsus4=";
-    })
-  ];
-
   nativeBuildInputs = [
     just
     pkg-config
     util-linuxMinimal
     libcosmicAppHook
+    rustPlatform.bindgenHook
   ];
 
   buildInputs = [
     dbus
     glib
     libinput
+    pipewire
     pulseaudio
     udev
   ];
